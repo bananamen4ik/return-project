68e2dccebc351244ba911b8949978d31265bf3ba
bananamen4ik <bananamen4ik@icloud.com>
2026-08-30 14:48:13 +0300

    Done

--- src/return_project/learn.py
+++ src/return_project/learn.py
@@ -1,82 +1,31 @@
-class Counter:
-    def __init__(self, limit):
-        self.current = 1
-        self.limit = limit
-
-    def __iter__(self):
-        return self
-
-    def __next__(self):
-        if self.current > self.limit:
-            raise StopIteration
-
-        value = self.current
-        self.current += 1
-        return value
+def count_to_three():
+    yield 1
+    yield 2
+    yield 3
 
 
-counter = Counter(3)
-
-print(next(counter))  # 1
-print(next(counter))  # 2
-
-for number in counter:
-    print(number)  # 3
-
-def __iter__(self):
-    return self  # self потому что возвращается сам экземпляр у которого реализовано внутри next и сохранены данные его
-
-class Counter:
-    def __init__(self, limit):
-        self.count = 1
-        self.limit = limit
-
-    def __iter__(self):
-        return self
-
-    def __next__(self):
-        if self.count > self.limit:
-            raise StopIteration
-
-        count = self.count
-        self.count += 1
-
-        return count
+counter = count_to_three()
+print(next(counter))
+print(next(counter))
+print(next(counter))
 
 
-counter = Counter(5)
+def count_to(limit):
+    count = 1
 
-for number in counter:
-    print(number)
-
-class NumberRange:
-    def __init__(self, low, high):
-        self.count = low
-        self.low = low
-        self.high = high
-
-    def __iter__(self):
-        return self
-
-    def __next__(self):
-        if self.count > self.high:
-            raise StopIteration
-
-        count = self.count
-        self.count += 1
-
-        return count
+    while count <= limit:
+        yield count
+        count += 1
 
 
-numbers = NumberRange(3, 7)
+counter = count_to(3)
+print(next(counter))
+print(next(counter))
+print(next(counter))
 
-for number in numbers:
-    print(number)
+counter = count_to(3)
+for i in counter:
+    print(i)
 
-counter = Counter(3)
-
-for number in counter:
-    print(number)
-
-for number in counter:
-    print(number)  # Ничего не выведется так как counter тот же самый итератор, внутри которого уже достигнут лимит в next и все последующие будут вызывать StopIteration
+# Генератор отличается от моего итератора тем, что здесь используются функции и конструкция yield, и даже не нужно самому вызывать StopIteration, при завершении next сам понимает
+# Да и насколько я знаю по сути генераторы нужны для оптимизации программы, чтобы не высчитывать все сразу, а по мере необходимости, когда как итераторы идут по конкретной готовой уже коллекции зачастую
