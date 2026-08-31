c7bf8f92642dfbee9da0b2a21063c9c31ffd860d
bananamen4ik <bananamen4ik@icloud.com>
2026-08-31 20:44:34 +0300

    Done

--- src/return_project/learn.py
+++ src/return_project/learn.py
@@ -1,127 +1,133 @@
-from dataclasses import dataclass
+class User:
+    def __init__(self, name: str):
+        self.name = name
+
+    def greet(self) -> str:
+        return f"Hello, {self.name}!"
 
 
-@dataclass
+class User:
+    def __init__(self, name: str, age: int):
+        self.name = name
+        self.age = age
+
+    @classmethod
+    def from_string(cls, value: str):
+        value_parts = value.split(",")
+        return cls(value_parts[0], int(value_parts[1]))
+
+
+user = User.from_string("Armen,27")
+print(user.name, user.age)
+
+
+class User:
+
+    @classmethod
+    def create(cls):
+        return cls()
+
+
+class Admin(User):
+    pass
+
+
+user = Admin.create()  # Вернется экземпляр Admin
+
+print(type(user))  # Admin, так как cls смотрит текущий класс который вызывается
+
+
+class MathUtils:
+    @staticmethod
+    def is_even(number):
+        return number % 2 == 0
+
+
+print(MathUtils.is_even(10))  # True
+print(MathUtils.is_even(7))  # False
+
+user.get_full_name() - обычный
+метод
+User.from_string("Armen,27") - classmethod
+User.calculate_something(10, 20) - staticmethod
+
+
+class User:
+    def __init__(self, first_name, last_name):
+        self.first_name = first_name
+        self.last_name = last_name
+
+    @property
+    def full_name(self):
+        return f"{self.first_name} {self.last_name}"
+
+
+user = User("Armen", "Doe")
+
+print(user.full_name)
+
+
 class Product:
-    name: str
-    price: float
-    quantity: int
+    def __init__(self, price):
+        self.price = price
+
+    @property
+    def price(self):
+        return self._price
+
+    @price.setter
+    def price(self, price):
+        if price < 0:
+            raise ValueError("Price cannot be negative")
+        self._price = price
 
 
-product = Product("MacBook", 2000, 2)
-
-print(product)
-
-from dataclasses import dataclass
+product = Product(100)
+product.price = -100
+print(product.price)
 
 
-@dataclass
-class Product:
-    name: str
-    price: float
-    quantity: int
-    is_available: bool = True
-
-
-product = Product("MacBook", 2000, 2)
-print(product)
-product = Product("MacBook", 2000, 2, False)
-print(product)
-
-from dataclasses import dataclass, field
-
-
-@dataclass
 class User:
-    name: str
-    skills: list[str] = field(default_factory=list)
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+
+    def __str__(self):
+        return f"{self.name}, {self.age}"
+
+    def __repr__(self):
+        return f"User(name='{self.name}', age={self.age})"
 
 
-user1 = User("Armen")
-user2 = User("John")
-
-user1.skills.append("Python")
-
-print(user1)
-print(user2)
+user = User("Armen", 27)
+print(user)
+print(repr(user))
 
 
-def calculate_total(price: float, quantity: int, discount: float) -> float:
-    ...
-
-
-def get_user(user_id: int) -> dict[str, str | int] | None:
-    ...
-
-
-# Означает что функция принимает user_id integer и возвращает либо (словарь где ключ это str, значение str или int), либо None
-
-from collections.abc import Iterable
-
-numbers: list[int] = [1, 2, 3, 4, 5]
-
-
-def get_even_numbers(numbers: Iterable[int]) -> list[int]:
-    ...
-
-
-from collections.abc import Callable
-
-
-def apply_operation(func: Callable[[int], int], number: int) -> int:
-    return func(number)
-
-
-def square(x: int) -> int:
-    return x ** 2
-
-
-print(apply_operation(square, 5))
-#
-# Разница между list[int], Iterable[int] и Iterator[int] в том что:
-# list[int] ожидается list с элементами int
-# Iterable[int] любой объект, у которого есть итератор и по нему можно пройтись и вернет int
-# Iterator[int] напрямую ожидается итератор который возвращает int
-#
-# почему list[int] уже конкретная структура данных, а Iterable[int] описывает более широкий набор объектов?
-# Потому что Iterable может выступать множество разных структур данных у которых есть итератор, а list это одна из
-
-from dataclasses import dataclass, field
-from collections.abc import Iterable
-
-
-@dataclass
 class User:
-    id: int
-    name: str
-    age: int
-    skills: list[str] = field(default_factory=list)
-    is_active: bool = True
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+
+    def __str__(self):
+        return f"{self.name}, {self.age}"
+
+    @classmethod
+    def from_string(cls, string):
+        name, age = string.split(",")
+        return cls(name, int(age))
+
+    @property
+    def is_adult(self):
+        return self.age >= 18
 
 
-def get_active_users(users: Iterable[User]) -> list[User]:
-    return list(users)
+user = User.from_string("Armen,27")
+print(user)
 
+# user.greet() отличается тем что вызывается экземпляр и работа происходит с self
+# User.from_string(...) здесь доступ есть только к cls если используется classmethod
+# User.is_something(...) если реализовано через staticmethod, то нет доступа к self и cls
 
-user1: User = User(1, "Armen", 27, skills=["Python", "Django"])
-user2: User = User(2, "John", 30)
-
-print(get_active_users((user1, user2)))
-
-
-@dataclass
-class User:
-    name: str
-    skills: list[str] = field(default_factory=list)
-
-
-user1 = User("Armen")
-user2 = User("John")
-
-user1.skills.append("Python")
-
-print(user1.skills)  # ["Python"]
-print(user2.skills)  # []
-
-# И почему здесь default_factory=list, а не просто skills: list[str] = []?
-# Потому что list это mutable коллекция и чтобы не сделать для всех единый список есть специальная функция field
+# Когда тебе нужен self, когда cls, а когда вообще ничего не нужно?
+# self если нужно взаимодействие с данными экземпляра, cls если только доступ к классу, если ни то, ни другое то staticmethod
