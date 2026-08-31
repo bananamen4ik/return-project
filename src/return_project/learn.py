07e64b87f0bb754a300f6d062d543227e228fd73
bananamen4ik <bananamen4ik@icloud.com>
2026-08-31 17:52:36 +0300

    Done

--- src/return_project/learn.py
+++ src/return_project/learn.py
@@ -1,116 +1,78 @@
-def generate_numbers():
-    count = 1
-    limit = 5
+users = ["Armen", "John", "Alex"]
 
-    while count <= limit:
-        yield count
-        count += 1
+for index, name in enumerate(users, start=1):
+    print(f"{index}. {name}")
 
+names = ["Armen", "John", "Alex"]
+ages = [27, 30, 25]
 
-gen = generate_numbers()
+print({
+    name: age
+    for name, age in zip(names, ages)
+})
 
-print(next(gen))
-print(next(gen))
-print(next(gen))
-print(next(gen))
-print(next(gen))
+ages = [15, 16, 17, 20]
 
-def countdown(n):
-    count = n
+print(any(map(lambda age: age >= 18, ages)))
 
-    while count > 0:
-        yield count
-        count -= 1
+ages = [20, 25, 30, 19]
 
+print(all(map(lambda age: age >= 18, ages)))
 
-for number in countdown(5):
-    print(number)
+numbers = [1, 2, 3, 4, 5]
+print(list(map(lambda x: x * 10, numbers)))
+# Тип до list generator хотел написать, затем проверил и type показывает map почему?
 
-def squares(numbers):
-    for number in numbers:
-        yield number ** 2
+numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 
+print(list(filter(lambda x: x % 2 == 0, numbers)))
 
-for number in squares([1, 2, 3, 4]):
-    print(number)
+users = [
+    {"name": "Armen", "age": 27},
+    {"name": "John", "age": 20},
+    {"name": "Alex", "age": 35},
+    {"name": "Maria", "age": 25},
+]
 
-def counter():
-    count = 0
+print(sorted(users, key=lambda x: x["age"], reverse=True))
 
-    while True:
-        yield count
-        count += 1
+numbers = [3, 1, 2]
 
+result = sorted(numbers)
 
-gen = counter()
+print(result)  # [1, 2, 3]
+print(numbers)  # [3, 1, 2]
+# Отличие от numbers.sort(), что numbers.sort() изменяет сам список, а sorted новый возвращает отсортированный
 
-print(next(gen))
-print(next(gen))
-print(next(gen))
-print(next(gen))
-print(next(gen))
+names = ["Armen", "John", "Alex"]
+scores = [95, 87, 91]
 
-numbers = (number ** 2 for number in range(1, 11) if number % 2 == 0)
-print(type(numbers))
+for index, user in enumerate(zip(names, scores), start=1):
+    print(f"{index}. {user[0]} - {user[1]}")
 
-for number in numbers:
-    print(number)
+a = [1, 2, 3]
+b = [4, 5]
+c = [6, 7]
 
-def test():
-    print("A")
-    yield 10
-    print("B")
-    yield 20
-    print("C")
+from itertools import chain
 
-# При присвоении gen = test() будет создан объект генератора и выполнен print("A")
-# Первый вызов next(gen) = вернет 10. Следующий вызов print("B") и вернет 20, следующий
-# print("C") и StopIteration исключение
+print(list(chain(a, b, c)))
 
-generator = test()
+numbers = [1, 2, 3, 4]
 
-print("X")
-print(next(generator))
-print("Y")
-print(next(generator))
-print("Z")
+result = map(lambda x: x * 2, numbers)
 
-# При присвоении generator = test() будет создан объект генератора и выполнен print("A")
-# затем print("X"), далее print("A") и yield 10, далее print("Y"), далее print("B") и yield 20,
-# далее print("Z")
+print(list(result))  # [2, 4, 6, 8]
+print(list(result))  # [] так как result уже полностью весь пройден предыдущим вызовом
 
-def counter():
-    value = 0
+users = [
+    {"name": "Armen", "age": 27, "active": True},
+    {"name": "John", "age": 17, "active": True},
+    {"name": "Alex", "age": 30, "active": False},
+    {"name": "Maria", "age": 25, "active": True},
+]
 
-    while value < 3:
-        yield value
-        value += 1
-
-
-generator = counter()
-
-print(next(generator))  # 0
-print(next(generator))  # 1
-print(next(generator))  # 2
-
-# И главное: почему value не сбрасывается в 0 при каждом next()? Потому что работаем внутри того же объекта generator
-# у которого при создании создалась и помнится переменная value, yield не завершает функцию
-
-numbers = [x * 2 for x in range(1_000_000)]
-numbers = (x * 2 for x in range(1_000_000))
-
-# Разница между ними в том что первый это list comprehension сразу создает список из миллиона элементов
-# второй это генератор, который создает только объект генератора и ждем вызова next, чтобы выдать следующий элемент
-# Что создаётся сразу? список
-# Где хранятся вычисленные значения? Внутри итератора
-# Что происходит при next() во втором варианте? Перебор элементов следующих
-# 1. Почему генератор может быть выгоднее по памяти? Потому что в память не загружаются все данные, а выдаются лениво по мере вызова next
-
-def read_numbers(numbers):
-    return (number for number in numbers if number > 10)
-
-
-numbers = [5, 15, 3, 20, 8, 30]
-
-for number in read_numbers(numbers):
-    print(number)
+print([
+    user["name"]
+    for user in filter(lambda user: (user["age"] >= 18 and user["active"]), users)
+])
