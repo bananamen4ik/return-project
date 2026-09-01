684a50ed780d23458faa01cdc22493d559db83ad
bananamen4ik <bananamen4ik@icloud.com>
2026-09-01 14:27:08 +0300

    Done

--- src/return_project/learn.py
+++ src/return_project/learn.py
@@ -1,160 +1,165 @@
 import asyncio
 
 
-# async def hello():
-#     await asyncio.sleep(1)
-#     return "Hello"
-#
-#
-# async def main():
-#     task = asyncio.create_task(hello())
-#
-#     print(task)
-#
-#     result = await task
-#
-#     print(result)
-#
-#
-# asyncio.run(main())
+async def get_number(number):
+    await asyncio.sleep(1)
+    return number
 
-# Что представляет собой task? Объект Task
-# Почему print(task) не выводит "Hello"? Потому что ссылка на объект Task. Чтобы получить результат
-# нужно дождаться выполнения Task через await надежно
-# 1. Что делает await task? Ожидает выполнения Task и возвращает результат корутины
+async def main():
+    results = await asyncio.gather(
+        get_number(1),
+        get_number(2),
+        get_number(3),
+    )
+
+    print(results)  # [1, 2, 3]
+
+# results тип list, займет 1 секунду
 
 async def first():
-    print("first start")
-    await asyncio.sleep(1)
-    print("first end")
-    return 1
+    await asyncio.sleep(3)
+    return "first"
 
 
 async def second():
-    print("second start")
     await asyncio.sleep(1)
-    print("second end")
-    return 2
+    return "second"
 
 
-#
-# async def main():
-#     task1 = asyncio.create_task(first())
-#     task2 = asyncio.create_task(second())
-#
-#     result1 = await task1
-#     result2 = await task2
-#
-#     print(result1, result2)
-
-# До запуска напиши предполагаемый порядок вывода.
-# print("first start"), print("second start"), print("first end"), print("second end"), print(result1, result2)
-
-# примерно сколько секунд будет выполняться программа?
-# 1 секунду
-
-# async def main():  # A
-#     await first()
-#     await second()
-#
-#
-# async def main():  # B
-#     task1 = asyncio.create_task(first())
-#     task2 = asyncio.create_task(second())
-#
-#     await task1
-#     await task2
-
-# Сколько примерно занимает A? - 2 секунды
-# Сколько примерно занимает B? - 1 секунда
-# Почему результат разный?
-# Потому что в А синхронно ждет программа выполнения поочередного await.
-# Во втором случае создаются один за другим через task корутины и выполняются примерно параллельно
-
-# async def foo():
-#     print("foo")
-#     await asyncio.sleep(1)
-#     print("foo done")
-
-
-# async def main():
-#     print("A")
-#
-#     task = asyncio.create_task(foo())
-#
-#     print("B")
-#
-#     await task
-#
-#     print("C")
-
-# Порядок: print("A"), print("foo"), print("B"), print("foo done"), print("C")
-# И объясни, почему foo не обязательно печатает что-либо прямо внутри create_task().
-# Этот момент я не понял, я думал что asyncio.create_task(foo()) выполняет код до await внутри foo
-
-# async def foo():
-#     return 42
-#
-#
-# async def main():
-#     task = asyncio.create_task(foo())
-#
-#     result = await task
-#
-#     print(result)  # 42
-#     print(task.done())  # True
-#     print(task.result())  # 42
-
-# async def foo():
-#     await asyncio.sleep(1)
-#     return 42
-#
-#
-# async def main():
-#     task = asyncio.create_task(foo())
-#
-#     print(task.done())  # False потому что еще не выполнилась корутина, там секунда пройти должна, а этот участок точно быстрее ее начнется
-#
-#     await task
-#
-#     print(task.done())  # True потому что после await гарантировано корутина уже выполнилась
-
-# Разница между coroutine = foo() и task = asyncio.create_task(foo()) и result = await foo()
-# coroutine = foo() - получает объект корутины
-# task = asyncio.create_task(foo()) - получает объект Task для дальнейшего отслеживания выполнения корутины
-# result = await foo()  - дожидается выполнения корутины и получает результат
-
-# async def
-#    ↓
-# foo()
-#    ↓
-# Coroutine
-#    ↓
-# create_task(...)
-#    ↓
-# Task
-#    ↓
-# await
-#    ↓
-# Coroutine value
-
-async def worker(name):
-    print(f"{name}: start")
+async def third():
     await asyncio.sleep(2)
-    print(f"{name}: end")
+    return "third"
+
+results = await asyncio.gather(
+    first(),
+    second(),
+    third(),
+)
+
+print(results)
+
+# results = ["first", "second", "third"]
+# время ≈ 3
+# в каком порядке фактически завершатся coroutine? - second, third, first
+
+async def get_name():
+    await asyncio.sleep(1)
+    return "Armen"
+
+
+async def get_age():
+    await asyncio.sleep(1)
+    return 27
+
+
+async def get_city():
+    await asyncio.sleep(1)
+    return "Amsterdam"
 
 
 async def main():
-    task1 = asyncio.create_task(worker("A"))
-    task2 = asyncio.create_task(worker("B"))
-    task3 = asyncio.create_task(worker("C"))
+    name, age, city = await asyncio.gather(
+        get_name(),
+        get_age(),
+        get_city(),
+    )
 
-    await task1
-    await task2
-    await task3
+    print(name, age, city)
 
-# Какой будет порядок start? A, B, C
-# Какой будет порядок end? A, B, C
-# Сколько примерно секунд? 2
-# Почему await task1 первым не заставляет task2 и task3 ждать своего запуска?
-# Потому что уже все запустились, await task1 лишь ждет конца выполнения своей задачи в не зависимости от других
+
+asyncio.run(main())
+
+# Объясни, почему здесь порядок переменных не зависит от того, какая coroutine завершилась первой.
+# Потому что gather отдает элементы в списке такой же последовательности, какой аргументы были переданы в него
+
+# A
+task1 = asyncio.create_task(first())
+task2 = asyncio.create_task(second())
+
+result1 = await task1
+result2 = await task2
+
+# B
+result1, result2 = await asyncio.gather(
+    first(),
+    second(),
+)
+
+# Будет ли разница во времени выполнения? Не будет
+# Что в итоге делает gather()? Помогает запускать без лишнего кода удобно конкурентно корутины и получать результаты
+# Что получаем в result1 и result2? "first" и "second"
+# Нужно ли нам вручную создавать Tasks в варианте B? Нет
+
+async def foo():
+    await asyncio.sleep(1)
+    return 10
+
+
+async def bar():
+    await asyncio.sleep(2)
+    return 20
+
+
+async def main():
+    results = await asyncio.gather(foo(), bar())
+
+    print(results)
+    print(type(results))
+
+# вывод: [10, 20]
+# тип: list
+# время: 2
+
+async def good():
+    await asyncio.sleep(1)
+    return "OK"
+
+
+async def bad():
+    await asyncio.sleep(1)
+    raise ValueError("Something went wrong")
+
+async def main():
+    results = await asyncio.gather(
+        good(),
+        bad(),
+    )
+
+    print(results)
+
+# Вернётся ли results? Я так понимаю исключение пойдет вверх и программа завершится
+# Что произойдёт с ValueError? Пробьется наружу и остановит выполнение программы
+# Напечатается ли results? Нет
+# 1. Что произойдёт с good()? Выполнится полностью
+
+results = await asyncio.gather(
+    good(),
+    bad(),
+    return_exceptions=True,
+)  # results = ["OK", ValueError("Something went wrong")]
+
+async def request(name, delay):
+    print(f"{name}: start")
+    await asyncio.sleep(delay)
+    print(f"{name}: end")
+    return name
+
+results = await asyncio.gather(
+    request("A", 3),
+    request("B", 1),
+    request("C", 2),
+)
+
+# 1. Порядок start? A, B, C
+# 2. Порядок end? B, C, A
+# 3. Что будет в results? ["A", "B", "C"]
+# 4. Сколько секунд? 3
+# 5. Почему порядок end и порядок results будут различаться?
+# Потому что порядок results зависит от порядка аргументов в gather, а end от времени sleep
+
+foo()
+create_task(foo())
+await foo()
+await asyncio.gather(foo(), bar())
+
+# Coroutine -> Task -> result coroutine -> result 2 coroutines
