8a2c597a17b5186f987fdba741b5b7097e21656b
bananamen4ik <bananamen4ik@icloud.com>
2026-09-01 13:25:11 +0300

    Done

--- src/return_project/learn.py
+++ src/return_project/learn.py
@@ -1,106 +1,160 @@
 import asyncio
 
 
-async def hello():
-    return "Hello"
+# async def hello():
+#     await asyncio.sleep(1)
+#     return "Hello"
+#
+#
+# async def main():
+#     task = asyncio.create_task(hello())
+#
+#     print(task)
+#
+#     result = await task
+#
+#     print(result)
+#
+#
+# asyncio.run(main())
 
-
-async def main():
-    print(await hello())
-
-
-asyncio.run(main())
-
-async def get_number():
-    return 42
-
-
-async def main():
-    result = await get_number()
-    print(result)
-    print(type(result))
-
-
-asyncio.run(main())
-
-async def first():
-    print("first")
-    return 1
-
-
-async def second():
-    print("second")
-    return 2
-
-
-async def main():
-    a = await first()
-    b = await second()
-
-    print(a, b)
-
-
-asyncio.run(main())
-
-# После запуска будет print("first"), return 1, print("second"), return 2, print(a, b)
-
+# Что представляет собой task? Объект Task
+# Почему print(task) не выводит "Hello"? Потому что ссылка на объект Task. Чтобы получить результат
+# нужно дождаться выполнения Task через await надежно
+# 1. Что делает await task? Ожидает выполнения Task и возвращает результат корутины
 
 async def first():
     print("first start")
     await asyncio.sleep(1)
     print("first end")
+    return 1
 
 
 async def second():
     print("second start")
     await asyncio.sleep(1)
     print("second end")
+    return 2
+
+
+#
+# async def main():
+#     task1 = asyncio.create_task(first())
+#     task2 = asyncio.create_task(second())
+#
+#     result1 = await task1
+#     result2 = await task2
+#
+#     print(result1, result2)
+
+# До запуска напиши предполагаемый порядок вывода.
+# print("first start"), print("second start"), print("first end"), print("second end"), print(result1, result2)
+
+# примерно сколько секунд будет выполняться программа?
+# 1 секунду
+
+# async def main():  # A
+#     await first()
+#     await second()
+#
+#
+# async def main():  # B
+#     task1 = asyncio.create_task(first())
+#     task2 = asyncio.create_task(second())
+#
+#     await task1
+#     await task2
+
+# Сколько примерно занимает A? - 2 секунды
+# Сколько примерно занимает B? - 1 секунда
+# Почему результат разный?
+# Потому что в А синхронно ждет программа выполнения поочередного await.
+# Во втором случае создаются один за другим через task корутины и выполняются примерно параллельно
+
+# async def foo():
+#     print("foo")
+#     await asyncio.sleep(1)
+#     print("foo done")
+
+
+# async def main():
+#     print("A")
+#
+#     task = asyncio.create_task(foo())
+#
+#     print("B")
+#
+#     await task
+#
+#     print("C")
+
+# Порядок: print("A"), print("foo"), print("B"), print("foo done"), print("C")
+# И объясни, почему foo не обязательно печатает что-либо прямо внутри create_task().
+# Этот момент я не понял, я думал что asyncio.create_task(foo()) выполняет код до await внутри foo
+
+# async def foo():
+#     return 42
+#
+#
+# async def main():
+#     task = asyncio.create_task(foo())
+#
+#     result = await task
+#
+#     print(result)  # 42
+#     print(task.done())  # True
+#     print(task.result())  # 42
+
+# async def foo():
+#     await asyncio.sleep(1)
+#     return 42
+#
+#
+# async def main():
+#     task = asyncio.create_task(foo())
+#
+#     print(task.done())  # False потому что еще не выполнилась корутина, там секунда пройти должна, а этот участок точно быстрее ее начнется
+#
+#     await task
+#
+#     print(task.done())  # True потому что после await гарантировано корутина уже выполнилась
+
+# Разница между coroutine = foo() и task = asyncio.create_task(foo()) и result = await foo()
+# coroutine = foo() - получает объект корутины
+# task = asyncio.create_task(foo()) - получает объект Task для дальнейшего отслеживания выполнения корутины
+# result = await foo()  - дожидается выполнения корутины и получает результат
+
+# async def
+#    ↓
+# foo()
+#    ↓
+# Coroutine
+#    ↓
+# create_task(...)
+#    ↓
+# Task
+#    ↓
+# await
+#    ↓
+# Coroutine value
+
+async def worker(name):
+    print(f"{name}: start")
+    await asyncio.sleep(2)
+    print(f"{name}: end")
 
 
 async def main():
-    await first()
-    await second()
+    task1 = asyncio.create_task(worker("A"))
+    task2 = asyncio.create_task(worker("B"))
+    task3 = asyncio.create_task(worker("C"))
 
-#  После запуска займет примерно 2 секунды из-за двух asyncio.sleep которые приостанавливают выполнение
+    await task1
+    await task2
+    await task3
 
-async def foo():
-    return 10
-
-result = foo()
-
-print(result)
-print(type(result))
-
-# Что находится в result? - Находится courutine object
-# Почему там не 10? - Потому что не вызывается с помощью await который начинает выполнение
-# Выполнялась ли foo()?  Нет, только создан объект корутины
-
-async def main(): # нужно было добавить async, так как await может использоваться только в асинхронном контексте
-    result = await get_data()
-    print(result)
-
-# def foo(): отличается от async def foo():, тем, что это обычная функция и при вызове она сразу выполняется
-# а async создает корутину и ждем await чтобы выполниться асинхронно
-
-# foo() вернет coroutine object, если объявлена с помощью async
-
-# await foo() выполняет функцию корутину
-
-# asyncio.run(main()) нужен чтобы запустить выполнение верхней корутины, я так понимаю запускатеся event loop
-
-async def foo():
-    print("A")
-    await asyncio.sleep(1)
-    print("B")
-
-
-async def main():
-    print("C")
-    await foo()
-    print("D")
-
-
-asyncio.run(main())
-
-# Запускается main(), print("C"), print("A"), ожидание 1 сек, print("B"), print("D")
-# Будет порядок: C, A, B, D. Такой порядок из-за вызовов выше
+# Какой будет порядок start? A, B, C
+# Какой будет порядок end? A, B, C
+# Сколько примерно секунд? 2
+# Почему await task1 первым не заставляет task2 и task3 ждать своего запуска?
+# Потому что уже все запустились, await task1 лишь ждет конца выполнения своей задачи в не зависимости от других
