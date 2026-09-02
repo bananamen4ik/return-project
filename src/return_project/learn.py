937ee8f669f60ca5251604c2a6bdfef82266a45f
bananamen4ik <bananamen4ik@icloud.com>
2026-09-02 12:58:41 +0300

    Done

--- src/return_project/learn.py
+++ src/return_project/learn.py
@@ -1,122 +1,98 @@
+try:
+    raise ExceptionGroup(
+        "test",
+        [
+            ValueError("value"),
+            TypeError("type"),
+        ],
+    )
+except* ValueError:
+    print("value caught")
+
+# Выведется print("value caught") и далее вылезет наружу TypeError("type")
+# Выведется ли value caught? да
+# Что произойдёт с TypeError? вылезет наружу
+# Завершится ли программа без необработанного исключения? нет
+
+try:
+    raise ExceptionGroup(
+        "test",
+        [
+            ValueError("value"),
+            TypeError("type"),
+        ],
+    )
+except* ValueError:
+    print("value caught")
+except* TypeError:
+    print("type caught")
+
+# Будет выведено: print("value caught"), print("type caught")
+
+try:
+    raise ExceptionGroup(
+        "test",
+        [
+            ValueError("A"),
+            TypeError("B"),
+            ValueError("C"),
+        ],
+    )
+except* ValueError as e:
+    print("caught:", type(e))
+    print("number:", len(e.exceptions))
+
+# какой будет type(e)? ExceptionGroup
+# чему будет равен len(e.exceptions)? 2
+# какие именно исключения окажутся внутри e? ValueError
+
 import asyncio
 
 
-async def worker(name, delay):
-    await asyncio.sleep(delay)
-    return name
-
-
-async with asyncio.TaskGroup() as tg:
-    task1 = tg.create_task(worker("A", 3))
-    task2 = tg.create_task(worker("B", 1))
-    task3 = tg.create_task(worker("C", 2))
-
-# 1. Сколько примерно времени? 3
-# 2. Все ли Tasks завершатся? да
-# 3. Что будет после async with? все задачи завершатся и программа продолжит работу
-# 4. Что вернёт task1.result()? A
-
-async with asyncio.TaskGroup() as tg:
-    task1 = tg.create_task(worker("A", 3))
-    task2 = tg.create_task(worker("B", 1))
-    task3 = tg.create_task(worker("C", 2))
-
-print(task1.result())
-print(task2.result())
-print(task3.result())
-
-
-# Вывод будет: A, B, C с каждой новой строки как print идет
-
-async def good(name, delay):
-    await asyncio.sleep(delay)
-    print(f"{name} finished")
-
-
-async def bad():
+async def worker(name, exc):
     await asyncio.sleep(1)
-    raise ValueError("error")
+    raise exc
 
 
-try:
-    async with asyncio.TaskGroup() as tg:
-        tg.create_task(good("A", 3))
-        tg.create_task(bad())
-        tg.create_task(good("C", 2))
-except* ValueError:
-    print("caught")
-
-# Что произойдёт через 1 секунду? Вызовется ValueError("error")
-# Успеет ли A вывести A finished? Нет
-# Успеет ли C вывести C finished? Нет
-# Что будет выведено после TaskGroup? print("caught")
-# Будут ли A и C отменены? Да
-
-# 4
-async def good(name, delay):
-    await asyncio.sleep(delay)
-    print(f"{name} finished")
-    return name
-
-
-async def bad():
-    await asyncio.sleep(1)
-    raise ValueError("error")
-
-# A
-await asyncio.gather(
-    good("A", 3),
-    bad(),
-    good("C", 2),
-)
-
-# B
-async with asyncio.TaskGroup() as tg:
-    tg.create_task(good("A", 3))
-    tg.create_task(bad())
-    tg.create_task(good("C", 2))
-
-# Что принципиально произойдёт с A и C при ошибке bad() в каждом варианте?
-# В варианте A: выполнятся и попадут в list
-# В варианте C: отменятся
-
-async with asyncio.TaskGroup() as tg:
-    tg.create_task(worker("A", 1))
-    tg.create_task(worker("B", 2))
-
-# Этот код не позволяет написать results = await ... как с gather, потому что здесь нужно сохранить
-# результаты в переменные и после async with получить результаты через .result()
-
-async def worker(name):
+async def main():
     try:
-        await asyncio.sleep(10)
-        print(name)
-    except asyncio.CancelledError:
-        print(f"{name} cancelled")
-        raise
+        async with asyncio.TaskGroup() as tg:
+            tg.create_task(worker("A", ValueError("A error")))
+            tg.create_task(worker("B", TypeError("B error")))
+    except* ValueError:
+        print("value")
+    except* TypeError:
+        print("type")
 
-try:
-    async with asyncio.TaskGroup() as tg:
-        tg.create_task(worker("A"))
-        tg.create_task(worker("B"))
 
-        await asyncio.sleep(1)
-        raise ValueError("boom")
-except* ValueError:
-    print("caught")
+asyncio.run(main())
 
-# Полный вывод, здесь я напишу как я вижу, но нужна будет помощь:
-# Сначала вызовется raise ValueError("boom"), затем A cancelled, B cancelled, print("caught")
-# но не знаю что будет с двумя asyncio.CancelledError которые пробрасываются наружу когда конкретно завершится программа
+# Что произойдёт с TaskGroup? вызовется ValueError("A error"), отменится worker B, и print("value")
+# Но это с учетом что worker B не успеет завершиться сам, но так как там и там 1 секунда выполнения,
+# то я предпологаю он тоже может завершиться и то CancelledError он выкенет свой TypeError и тогда и его ошибка обработается
+# Почему здесь можно использовать два except*? потому что отдельно обрабатываются две ошибки
+# Что выведется? print("value")
+# Будут ли ValueError и TypeError существовать как два отдельных исключения, или наружу выйдет ExceptionGroup?
+# выйдет ExceptionGroup
 
-# 7
-# gather - выполнить корутины и вернуть результаты в list
-# TaskGroup - сгруппировать таски, поставить на выполнение и автоматически отменить в случае ошибки одного из
+# 5
+async with asyncio.TaskGroup() as tg:
+    tg.create_task(worker("A", ValueError("A")))
+    tg.create_task(worker("B", TypeError("B")))
 
-# 8
-# Нужно одновременно:
-# - получить пользователя из PostgreSQL
-# - получить настройки из Redis
-# - запросить данные внешнего API
+# A → ValueError добавляются в очередь
+# B → TypeError добавляются в очередь
+#       ↓
+# TaskGroup вызывает
+#       ↓
+# ExceptionGroup обе ошибки перехватываются и добавляются в группу исключений
+#       ↓
+# except* ValueError по отдельности обрабатываются
+# except* TypeError по отдельности обрабатываются
 
-# я бы выбрал gather, так как в случае ошибки он не прервет оставшиеся задачи и вернет все результаты в том числе и errors
+# Почему TaskGroup вообще нужен ExceptionGroup, если обычный except уже умеет ловить исключения?
+# Потому что может во время работы перехватиться не одна ошибка, а несколько
+
+# 6
+# Сравнить except ValueError:  и except* ValueError:
+# except работает с одним исключением, а except* отбирает конкретные типы исключений из группы
